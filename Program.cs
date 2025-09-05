d0a1cb6503d83c39b9a6ee6e470ff7b6c463fcc2
Ava Manning <362579849@GoTVDSB.ca>
2025-09-05 09:40:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic___2_Numbers_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("I will now count my chickens:");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+
+
+            Console.WriteLine("Six" + 6);
+            Console.WriteLine("6 + 6 ");
+            Console.WriteLine("6" + 6);
+            Console.WriteLine(6 + 6);
+
+
+            Console.WriteLine(" Now i will count the eggs:");
+            Console.WriteLine(11.0 / 2);
+
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine(" What is 3 + 2? " + (3 + 2));
+            Console.WriteLine(" What is 5 - 7?" + (5 - 7));
+            Console.WriteLine(" Oh thats why its " + (3 + 2 < 5 - 7));
+
+        }
+    }
+}
